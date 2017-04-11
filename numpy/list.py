8c6fc6b2c7f82403f4364aca6cac6607cce2b301
lovetube <lovetube@lovetube-Mac-mini.local>
2017-04-11 09:08:39 +0900

    init commit

---
+++ numpy/list.py
@@ -0,0 +1,10 @@
+import csv
+import numpy as np
+
+
+with open("winequality-red.csv", 'r') as f:
+	wines =  list(csv.reader(f, delimiter=";"))
+wines = np.array(wines[1:], dtype=np.float)
+
+print(wines)
+print(wines.shape)
